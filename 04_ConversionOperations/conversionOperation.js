8de93c3a71159e9392e1d1b5c02854afd9f9fd03
Leonidus786 <abhi.upadh@gmail.com>
2025-10-26 13:00:20 +0530

    Datatypes and operations

---
+++ 04_ConversionOperations/conversionOperation.js
@@ -0,0 +1,40 @@
+// let score = "33abc";
+// let score = null;
+// let score = undefined;
+// let score = true;
+// let score = false;
+// let score = "abhishek";
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan but type -- number
+// true => 1; false=>0
+
+// let isLoggedIn = 1
+
+// let isLoggedIn = "";
+
+// let isLoggedIn = "abhishek";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// "" => false; "abhi" => true
+// 1 => true; 0 => false
+
+let someNumber = 35;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
---
+++ 04_ConversionOperations/operations.js
@@ -0,0 +1,60 @@
+// operations
+
+let value = 3;
+
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 4);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "Abhishek";
+let str2 = " Upadhayay";
+
+let str3 = str1 + str2;
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// console.log(((3 + 4) * 5) % 3);
+
+// console.log(+true);
+// console.log(+false);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+
+// gameCounter++;
+++gameCounter;
+
+// console.log(gameCounter);
+
+// links
+
+// https://tc39.es/ecma262/#sec-toprimitive
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
